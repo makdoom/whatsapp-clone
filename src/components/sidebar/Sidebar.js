7c1009d457debc5b377f8b68e308a95246b4b832
Makdoom Shaikh <makshaikh99@gmail.com>
2021-01-09 12:04:58 +0530

    added css global variables

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,15 +1,44 @@
 import React from "react";
-import { Avatar } from "@material-ui/core";
+import { Avatar, IconButton } from "@material-ui/core";
+import Brightness7Icon from "@material-ui/icons/Brightness7";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchIcon from "@material-ui/icons/Search";
 import "./sidebar.css";
+
+import SidebarChat from "../sidebarChat/SidebarChat";
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar__header">
-        <Avatar />
+        <Avatar className="avatar" />
+        <div className="sidebar__header__right">
+          <IconButton>
+            <Brightness7Icon className="darkmode__icon" />
+          </IconButton>
+          <IconButton>
+            <ChatIcon className="chat__icon" />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon className="more__icon" />
+          </IconButton>
+        </div>
       </div>
-      <div className="sidebar__search"></div>
-      <div className="sidebar__chat"></div>
+
+      <div className="sidebar__search">
+        <div className="search__box">
+          <SearchIcon />
+          <input
+            type="text"
+            className="search__field"
+            placeholder="Search or start new chat"
+          />
+        </div>
+      </div>
+      <div className="sidebar__chat">
+        <SidebarChat />
+      </div>
     </div>
   );
 };
---
+++ src/components/sidebarChat/SidebarChat.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./sidebarchat.css";
+
+const SidebarChat = () => {
+  return <div className="sidebar__chat">Sidebar</div>;
+};
+
+export default SidebarChat;
